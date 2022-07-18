1d604ff9181eff945437d12544a287ed1cd169ac
Unai Garcia <unaigarciaruizd@gmail.com>
2022-07-18 13:41:48 +0200

    Correcciones

--- src/compass.rs
+++ src/compass.rs
@@ -1,4 +1,4 @@
-use accelerometer;
+use accelerometer::RawAccelerometer;
 use lsm303dlhc;
 
 use crate::hal::gpio;
@@ -10,18 +10,19 @@ use crate::hal::rcc;
 use crate::hal::stm32;
 
 use embedded_hal;
-use embedded_hal::digital::v2::OutputPin;
+
+const SCALE: f32 = 4.6 / 256.0;
 
 type I2c1 = i2c::I2c<
     stm32::I2C1,
     (
-        gpiob::PB6<gpio::Alternate<gpio::AF4>>,
-        gpiob::PB7<gpio::Alternate<gpio::AF4>>,
+        gpiob::PB6<gpio::AlternateOD<gpio::AF4>>,
+        gpiob::PB7<gpio::AlternateOD<gpio::AF4>>,
     ),
 >;
 
 pub struct Compass {
-    lsm303dlhc: lsm303dlhc::lsm303dlhc<I2c1>,
+    lsm303dlhc: lsm303dlhc::Lsm303dlhc<I2c1>,
 }
 
 impl Compass {
@@ -42,31 +43,23 @@ impl Compass {
             .internal_pull_up(true)
             .set_open_drain();
 
-        let i2c_mode = i2c::Mode {
-            polarity: i2c::Polarity::IdleLow,
-            phase: i2c::Phase::CaptureOnFirstTransition,
-        };
+        let i2c = i2c::I2c::new(i2c1, (scl, sda), 400.khz().into(), clocks);
 
-        let i2c = i2c::I2c::i2c1(i2c1, (scl, sda), i2c_mode, 400.khz().into(), clocks);
-
-        let config = lsm303dlhc::Config {
-            scale: lsm303dlhc::Scale::PlusMinus8G,
-            ..Default::default()
-        };
-        let lsm303dlhc = lsm303dlhc::lsm303dlhc::new(i2c, config);
+        //let lsm303dlhc = lsm303dlhc::Lsm303dlhc::new(i2c);
+        let mut lsm303dlhc = lsm303dlhc::Lsm303dlhc { i2c };
 
         Self { lsm303dlhc }
     }
 }
 
-impl compass::RawAccelerometer<accelerometer::vector::I8x3> for lsm303dlhc {
+impl accelerometer::RawAccelerometer<accelerometer::vector::I8x3> for Compass {
     type Error = i2c::Error;
     fn accel_raw(
         &mut self,
     ) -> Result<accelerometer::vector::I8x3, accelerometer::Error<Self::Error>> {
-        let x = self.read_accel_registers(accel::Registers::OUT_X_L_A);
-        let y = self.read_accel_registers(accel::Registers::OUT_Y_L_A);
-        let z = self.read_accel_registers(accel::Registers::OUT_Z_L_A);
+        let x = self.read_accel_registers(OUT_X_L_A);
+        let y = self.read_accel_registers(OUT_Y_L_A);
+        let z = self.read_accel_registers(OUT_Z_L_A);
         Ok(accelerometer::vector::I8x3::new(
             i8::from_le_bytes([x]),
             i8::from_le_bytes([y]),
@@ -75,11 +68,11 @@ impl compass::RawAccelerometer<accelerometer::vector::I8x3> for lsm303dlhc {
     }
 }
 
-impl compass::Accelerometer for lsm303dlhc {
-    type Error = spi::Error;
+impl accelerometer::Accelerometer for Compass {
+    type Error = i2c::Error;
     fn sample_rate(&mut self) -> Result<f32, accelerometer::Error<Self::Error>> {
-        self.modify_accel_register(accel::Register::CTRL_REG1_A, |r| {
-            r & !(0b1111 << 4) | ((odr as u8) << 4)
+        self.modify_accel_register(CTRL_REG1_A, |r| {
+            r & !(0b1111 << 4) | ((lsm303dlhc::AccelOdr as u8) << 4)
         })
     }
 
@@ -94,3 +87,4 @@ impl compass::Accelerometer for lsm303dlhc {
         ))
     }
 }
+
